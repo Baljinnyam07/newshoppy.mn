08776e76d829b256c643149e799e29ba2c0f8509
baljinnyam.html@gmail.com <baljinnyam.html@gmail.com>
2023-03-31 00:22:16 +0800

    commit

---
+++ server/data/Product.js
@@ -0,0 +1,12 @@
+const products = [
+    {
+        _id:"1",
+        name:"Velcro Ballerinas For Girls (Pink)",
+        image:"/images/6,png",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        price:89,
+        contInStock:2,
+        rating:4,
+        numReviews:4,
+   }
+]
